ac5e79e8caa2702aab23d7cecb24d3f3b38c0467
jen89 <jenchik_1989@bk.ru>
2021-04-08 19:47:49 +0700

    new

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function () {
+
+   $('.reviews-slider').slick({
+      infinite: true,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      dots: true,
+      arrows: false,
+      responsive: [
+         {
+            breakpoint: 1110,
+            settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1,
+            }
+         },
+      ]
+   });
+
+   $('.btn__menu').on('click', function () {
+      $('.menu__list').slideToggle();
+   });
+});
